f17fa255eeebbc89c19495b6b70063afa93e4f76
Volodya Oliynik <volodyaoliynik@gmail.com>
2021-11-06 15:25:33 +0200

    uptade t6

--- j3/t6/login.js
+++ j3/t6/login.js
@@ -2,7 +2,30 @@ function login() {
      const email = document.getElementById('email');
      const password = document.getElementById('password');
 
-     db.collection('users').get().where('email', '===', email.value).then((res)=>{
-               console.log(res.doc())
+     db.collection('users')
+     .where('email', '==', email.value)
+     .get()
+     .then((res)=>{
+               res.forEach(doc => {
+               let user  = {
+                    id: doc.id,
+                    ...doc.data()
+               }
+               if(user.password == password.value){
+                    localStorage.setItem('user', JSON.stringify(user));
+                    window.location.href = 'index.html';
+               }else {
+                    alert('Невірний пароль')
+               }
+               console.log(user)   
+               })
+               
      });
+}
+function checkUser() {
+     const user = localStorage.getItem('user');
+     console.log(user)
+     if(user != null || user != ''){
+          window.location.href = 'index.html';
+     }
 }
--- j3/t6/main.js
+++ j3/t6/main.js
@@ -5,3 +5,41 @@ function isLoged() {
      }
 }
 isLoged();
+
+let articles = [];
+
+function getArticles() {
+     db.collection('articles').get().then(res => {
+          res.forEach ( doc=>{
+               let article = {
+                    id: doc.id,
+                    ...doc.data()
+               }
+               articles.push(article);
+                drawArticles(article.id, article.title);
+          } )
+         
+     })
+}
+getArticles();
+function drawArticles(id, title) {
+     const box = document.createElement('div');
+     const boxImage = document.createElement('div');
+     const boxTitle = document.createElement('div');
+
+     box.classList.add('box');
+     box.classList.add('d-flex');
+     box.classList.add('mb-3');
+     box.id = id;
+
+     boxImage.classList.add('box-image');
+
+     boxTitle.classList.add('box-title');
+
+     boxTitle.innerText = title;
+
+     box.appendChild(boxImage);
+     box.appendChild(boxTitle);
+
+     document.getElementById('articles-box').appendChild(box);
+}
